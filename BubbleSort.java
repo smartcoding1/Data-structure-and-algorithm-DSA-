b03006587dc72e866ba9f042062100bcd559f34e
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-07-11 13:28:01 -0700

    Update BubbleSort.java

--- BubbleSort.java
+++ BubbleSort.java
@@ -1,27 +1,35 @@
-public class MyClass {
-
-   public static int [] BubbleSort(int x[])
-
-    {   int size=x.length;
-        for(int i=0;i<=size-2;i++)
-        {
-            for( int j=i+1;j<=size-1;j++)
-            {
-                if(x[i]>x[j])
+class BubbleSort
+{
+    void bubbleSort(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
                 {
-                    int tem=x[i];
-                    x[i]=x[j];
-                    x[j]=tem;
+                    // swap arr[j+1] and arr[j]
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
                 }
-
-            }
-        } return x;
     }
+ 
+    /* Prints the array */
+    void printArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i=0; i<n; ++i)
+            System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+ 
+    // Driver method to test above
     public static void main(String args[])
     {
-
-      int []a=bubbleSort(new int[] {5,6,1,7,2,9});
-      for( int t: a )
-      {
-          System.out.println(t);
-      }
+        BubbleSort ob = new BubbleSort();
+        int arr[] = {64, 34, 25, 12, 22, 11, 90};
+        ob.bubbleSort(arr);
+        System.out.println("Sorted array");
+        ob.printArray(arr);
+    }
+}
