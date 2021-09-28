e92b01811131a130275a99b86fb233345929c426
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-09-28 03:38:10 -0700

    max depth count of parenthesis lcode1614

---
+++ Solution40.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxDepth(String s)
+    {
+        int count=0;
+        int max=0;
+        for(int i=0;i<s.length();i++)
+        {
+            if(s.charAt(i)=='(')
+            {
+              count++;   
+              max=Math.max(max,count);
+            }
+            else if(s.charAt(i)==')')
+                count--;
+        }
+        return max;
+    }
+}
