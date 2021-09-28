48450220c51b53405a9f444f15beb95aa98216e1
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-09-28 03:08:01 -0700

    print all subarrays.

---
+++ Solution39.java
@@ -0,0 +1,18 @@
+public class MyClass {
+    public static void main(String args[]) 
+    {
+        int arr[]= {1,2,3,4};
+        for(int i=0;i<arr.length;i++)
+        {
+          for(int j=i;j<arr.length;j++)
+          {
+              for(int k=i;k<=j;k++)
+              {
+                  System.out.print(arr[k]);
+              }
+             System.out.println("");
+          }
+        }
+    }
+      
+}
