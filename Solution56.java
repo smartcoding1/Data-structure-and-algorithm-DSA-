49521054ab1b77849f3bf5c5416d577819a15815
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-10-07 14:47:10 -0700

    plus one leetcode66

---
+++ Solution56.java
@@ -0,0 +1,14 @@
+class Solution {
+   public static int[] plusOne(int[] digits) {
+        if(0==digits.length) return digits;
+	    for(int i=digits.length-1;i>=0;i--)
+	    {
+	        
+	       if(digits[i]!=9) {digits[i]++;return digits;}
+	   	   else	digits[i]=0;
+	    }
+	  int[] result=new int[digits.length+1];
+	  result[0]=1; 
+	  return result;   
+    }
+}
