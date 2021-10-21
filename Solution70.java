9c5f4ba316314a4c3b47d643308aa8f6106c76e4
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-10-21 23:53:51 +0530

    duplicate zero leetcode 1089

---
+++ Solution70.java
@@ -0,0 +1,21 @@
+ class Solution {
+    public void duplicateZeros(int[] arr)
+    { 
+     
+      for(int i=0;i<arr.length;i++)
+      {   
+          if(arr[i]==0)
+          { 
+             int j=arr.length-1;
+             while(j>i)
+             {
+                 arr[j]=arr[j-1];
+                 j--;
+             }
+              
+             i++;
+          }
+      }
+               
+    }
+}
