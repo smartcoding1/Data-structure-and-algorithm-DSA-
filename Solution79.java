06c43e3aaf11537f55fd8b0612451d71c562d775
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-10-25 21:45:12 +0530

    leetcode 1588

---
+++ Solution79.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int sumOddLengthSubarrays(int[] arr) {
+        int sum = 0;
+        for (int i=0; i<arr.length; i++) {
+            int temp = 0;
+            for (int j=i; j<arr.length; j++) {
+                temp += arr[j];
+                if ((j - i + 1) % 2 == 1)
+                    sum += temp;
+            }
+        }
+        return sum;
+            
+    }
+        
+}
