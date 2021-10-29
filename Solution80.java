584b86070b366b11ee5ecc9d88575e9218948559
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-10-29 17:59:24 +0530

    self divinding number

---
+++ Solution80.java
@@ -0,0 +1,23 @@
+class Solution {
+    public List<Integer> selfDividingNumbers(int left, int right) {
+     List<Integer>res=new ArrayList();
+        for(int i=left;i<=right;i++)
+        {
+            if(isDivide(i))
+                res.add(i);
+        }
+        return res;
+    }
+    public boolean isDivide(int num)
+    {
+        int n=num;
+        while(n>0)
+        {
+            int rem=n%10;
+            if(rem==0 ||(num%rem)!=0)
+                return false;
+            n=n/10; 
+        }
+        return true;
+    }
+}
