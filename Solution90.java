c58a6f263d1edbe3d95d7ad50ac1e4df596f4752
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2021-11-12 00:02:50 +0530

    two sum

---
+++ Solution90.java
@@ -0,0 +1,32 @@
+class Solution{
+public int[] twoSum(int[] nums, int target) {
+    /*for (int i = 0; i < nums.length; i++)
+    {
+        for (int j = i + 1; j < nums.length; j++) 
+        {
+            if (nums[j] == target - nums[i])
+            {
+                return new int[] { i, j };
+            }
+        }
+        
+    }*/
+    
+    int n=nums.length;
+    Map <Integer,Integer> h= new HashMap<>();
+    for(int i=0;i<n;i++)
+    {
+        if(h.containsKey(target-nums[i]))
+            return new int[]{h.get(target-nums[i]), i};
+        else
+        {
+            h.put(nums[i],i);
+        }
+        
+    }
+   
+        
+            return new int[]{0,0};
+   
+}
+}
