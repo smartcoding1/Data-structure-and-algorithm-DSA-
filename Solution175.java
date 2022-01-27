4838c8a939768fcf0a4afb6c46585a1954e010a8
Shakib mirza <56229196+smartcoding1@users.noreply.github.com>
2022-01-28 01:26:11 +0530

    reverse linked list itreative method leetcode206

---
+++ Solution175.java
@@ -0,0 +1,17 @@
+class Solution
+{
+    public ListNode reverseList(ListNode head)
+    {
+        ListNode pre=null;
+        ListNode curr=head;
+        ListNode next;
+        while(curr!=null)
+        {   
+            next=curr.next;
+            curr.next=pre;
+            pre=curr;
+            curr=next;
+        }
+        return pre;
+    }
+}
